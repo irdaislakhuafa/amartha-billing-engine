60246ed71d07c7959d569a326f819b6c74ea8496
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2025-05-17 20:14:12 +0700

    feat(loanbilling): implement get and delete loan billing feature

--- src/business/domain/loanbilling/loanbilling.go
+++ src/business/domain/loanbilling/loanbilling.go
@@ -12,13 +12,14 @@ import (
 	"github.com/irdaislakhuafa/go-sdk/convert"
 	"github.com/irdaislakhuafa/go-sdk/errors"
 	"github.com/irdaislakhuafa/go-sdk/log"
+	"github.com/irdaislakhuafa/go-sdk/querybuilder/sqlc"
 )
 
 type (
 	Interface interface {
 		Create(ctx context.Context, params entity.CreateLoanBillingParams) (entity.LoanBilling, error)
 		List(ctx context.Context, params any) ([]entity.LoanBilling, entity.Pagination, error)
-		Get(ctx context.Context, params any) (entity.LoanBilling, error)
+		Get(ctx context.Context, params entity.GetLoanBillingParams) (entity.LoanBilling, error)
 		Update(ctx context.Context, params entity.UpdateLoanBillingParams) (entity.LoanBilling, error)
 		Delete(ctx context.Context, params entity.DeleteLoanBillingParams) (entity.LoanBilling, error)
 		WithTx(ctx context.Context, tx *sql.Tx) Interface
@@ -77,20 +78,51 @@ func (i *impl) Create(ctx context.Context, params entity.CreateLoanBillingParams
 // Delete implements Interface.
 func (i *impl) Delete(ctx context.Context, params entity.DeleteLoanBillingParams) (entity.LoanBilling, error) {
 	args := entitygen.DeleteLoanBillingParams{
-		IsDeleted: 0,
+		IsDeleted: params.IsDeleted,
 		DeletedAt: sql.NullTime{},
 		DeletedBy: sql.NullString{},
-		ID:        0,
+		ID:        params.ID,
 	}
+
+	_, err := i.queries.DeleteLoanBilling(ctx, args)
+	if err != nil {
+		return entity.LoanBilling{}, errors.NewWithCode(codes.CodeSQLTxExec, err.Error())
+	}
+
+	result := entity.LoanBilling{
+		Base: entity.Base{
+			ID:        args.ID,
+			DeletedAt: convert.SQLNullTimeToNil(args.DeletedAt),
+			DeletedBy: convert.SQLNullStringToNil(args.DeletedBy),
+			IsDeleted: args.IsDeleted,
+		},
+	}
+
+	return result, nil
 }
 
 // Get implements Interface.
-func (i *impl) Get(ctx context.Context, params any) (entity.LoanBilling, error) {
-	panic("unimplemented")
+func (i *impl) Get(ctx context.Context, params entity.GetLoanBillingParams) (entity.LoanBilling, error) {
+	ctx = sqlc.Build(ctx, func(b *sqlc.Builder) {
+		b.And("id = ?", params.ID)
+		b.And("is_deleted = ?", params.IsDeleted)
+	})
+
+	row, err := i.queries.GetLoanBilling(ctx)
+	if err != nil {
+		return entity.LoanBilling{}, errors.NewWithCode(codes.CodeSQLTxExec, err.Error())
+	}
+
+	result, err := i.rowToEntity(row)
+	if err != nil {
+		return entity.LoanBilling{}, errors.NewWithCode(errors.GetCode(err), err.Error())
+	}
+
+	return result, nil
 }
 
 // List implements Interface.
-func (i *impl) List(ctx context.Context, params any) ([]entity.LoanBilling, entity.Pagination, error) {
+func (i *impl) List(ctx context.Context, params entity.ListLoanBillingParams) ([]entity.LoanBilling, entity.Pagination, error) {
 	panic("unimplemented")
 }
 
@@ -102,4 +134,25 @@ func (i *impl) Update(ctx context.Context, params entity.UpdateLoanBillingParams
 // WithTx implements Interface.
 func (i *impl) WithTx(ctx context.Context, tx *sql.Tx) Interface {
 	panic("unimplemented")
+}
+
+func (i *impl) rowToEntity(row entitygen.LoanBilling) (entity.LoanBilling, error) {
+	return entity.LoanBilling{
+		LoanTransactionID:   row.LoanTransactionID,
+		BillDate:            row.BillDate,
+		PrincipalAmount:     row.PrincipalAmount,
+		PrincipalAmountPaid: row.PrincipalAmountPaid,
+		InterestAmount:      row.InterestAmount,
+		InterestAmountPaid:  row.InterestAmountPaid,
+		Base: entity.Base{
+			ID:        row.ID,
+			CreatedAt: row.CreatedAt,
+			CreatedBy: row.CreatedBy,
+			UpdatedAt: convert.SQLNullTimeToNil(row.UpdatedAt),
+			UpdatedBy: convert.SQLNullStringToNil(row.UpdatedBy),
+			DeletedAt: convert.SQLNullTimeToNil(row.DeletedAt),
+			DeletedBy: convert.SQLNullStringToNil(row.DeletedBy),
+			IsDeleted: row.IsDeleted,
+		},
+	}, nil
 }
